ddc93567d92698a0b3e59021879279ca1521b975
brightcodedev <info@brightcode.dev>
2020-04-27 09:51:47 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { UserDetailComponent } from './components/user/user-detail/user-detail.component';
+import { UserFormComponent } from './components/user/user-form/user-form.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
+import { EventFormComponent } from './components/event/event-form/event-form.component';
+import { EventListComponent } from './components/event/event-list/event-list.component';
+import { EventDetailComponent } from './components/event/event-detail/event-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users/form', component: UserFormComponent },
+  { path: 'users/list', component: UserListComponent },
+  { path: 'users/:userId', component: UserDetailComponent },
+  { path: 'events/form', component: EventFormComponent },
+  { path: 'events/list', component: EventListComponent },
+  { path: 'events/:eventId', component: EventDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
+import { UserDetailComponent } from './components/user/user-detail/user-detail.component';
+import { UserFormComponent } from './components/user/user-form/user-form.component';
+import { EventListComponent } from './components/event/event-list/event-list.component';
+import { EventDetailComponent } from './components/event/event-detail/event-detail.component';
+import { EventFormComponent } from './components/event/event-form/event-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserFormComponent,    
+    EventListComponent,
+    EventDetailComponent,
+    EventFormComponent
   ],
   imports: [
     BrowserModule,
